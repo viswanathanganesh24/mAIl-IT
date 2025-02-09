8b5021b3b5480202584b01422d74b2832bf46e44
Viswanathan Ganesh <viswanathanganesh24@gmail.com>
2025-02-09 22:27:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
+import asyncio
 import streamlit as st
 import google.generativeai as genai
 import base64
+from httpx_oauth.clients.google import GoogleOAuth2
 from email.mime.text import MIMEText
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google_auth_oauthlib.flow import Flow
+from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
+
+client_id = st.secrets['client_id']
+client_secret = st.secrets['client_secret']
+redirect_uri = st.secrets['redirect_uri']
+client = GoogleOAuth2(client_id, client_secret)
+
 SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
 
 st.set_page_config(page_title="mAIl-IT", page_icon=":speech_balloon:", layout="centered")
 
-system_prompt = "You are a professional formal email writer. Just write a mail with the given information. Don't suggest anything in the mail. Make subject professional."
+system_prompt = st.secrets['system_prompt']
 
 if 'email' not in st.session_state:
     st.session_state['email'] = ""
@@ -25,12 +32,7 @@ if 'output' not in st.session_state:
 
 
 def AI(user_prompt):
-    # client = genai.Client(api_key="AIzaSyBOvwQexlkM5UFLEDv6si9xj2wfikGvmt0")
-    # response = client.models.generate_content(
-    # model="gemini-2.0-flash", contents=user_prompt
-    # )
-    # return response.text
-    genai.configure(api_key="AIzaSyBOvwQexlkM5UFLEDv6si9xj2wfikGvmt0")
+    genai.configure(api_key=st.secrets['genai_api_key'])
     model = genai.GenerativeModel('gemini-2.0-flash')
     response = model.generate_content(user_prompt)
     return response.text
@@ -40,12 +42,34 @@ def message_maker(content):
     p = content.split("\n")
     return p[0][9:], '\n'.join(p[2:])
 
-def sendMail(to_email, content):
-    #flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-    flow = InstalledAppFlow.from_client_config({"web":{"client_id":"942737272714-nsb4kb40i14b9820m0a4i2uqr3qs20no.apps.googleusercontent.com","project_id":"gen-lang-client-0112955057","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"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","redirect_uris":["https://mail-it.streamlit.app","http://localhost:8501"],"javascript_origins":["https://mail-it.streamlit.app","http://localhost:8501"]}}, SCOPES)
-    creds = flow.run_local_server(port=0)
+async def get_authorization_url(client: GoogleOAuth2, redirect_uri: str):
+    authorization_url = await client.get_authorization_url(redirect_uri, scope=SCOPES)
+    return authorization_url
 
-    service = build('gmail', 'v1', credentials=creds)
+auth_url = asyncio.run(
+    get_authorization_url(client, redirect_uri))
+
+async def get_access_token(client: GoogleOAuth2, redirect_uri: str, code: str):
+    token = await client.get_access_token(code, redirect_uri)
+    return token
+
+def sendMail(to_email, content):
+    service = None
+    if 'code' in st.query_params:
+        code = st.query_params['code']
+        st.write(code)
+        token = asyncio.run(get_access_token(client, redirect_uri, code))
+        st.write(token)
+        creds = Credentials(
+            token=token['access_token'],
+            refresh_token=token.get('refresh_token'),
+            token_uri="https://oauth2.googleapis.com/token",
+            client_id=client.client_id,
+            client_secret=client.client_secret,
+            scopes=SCOPES
+        )
+
+        service = build('gmail', 'v1', credentials=creds)
 
     subj, body = message_maker(content)
 
@@ -64,34 +88,36 @@ def sendMail(to_email, content):
 if 'submitted' not in st.session_state:
     st.session_state['submitted'] = False
 
-if not st.session_state["submitted"]:
+if 'signedIn' not in st.session_state:
+    st.session_state['signedIn'] = False
+
+if not st.session_state["submitted"] and not st.session_state["signedIn"]:
+
+    st.link_button("Sign In", auth_url)
+
+    if 'code' in st.query_params:
+        st.session_state['signedIn'] = True
+
+if not st.session_state["submitted"] and st.session_state['signedIn']:
     with st.form(key='my_form'):
         to = st.text_input("Name")
         st.session_state['to_email'] = st.text_input("To Email")
         reason = st.text_area("Describe about your mail requirements")
         name = st.text_input("Your Name")
-        #st.session_state['email'] = st.text_input("Your Email")
-        #st.session_state['pw'] = st.text_input("Email's Password", type="password")
         contact = st.text_input("Enter Phone number")
         submit = st.form_submit_button("Submit")
-
     if submit:
         user_prompt = system_prompt + " Write a very human email to " + to + " for the reason of: " + reason + ". My name is " + name + ". My Contact is " + contact
         st.session_state['output'] = AI(user_prompt=user_prompt)
         st.session_state['submitted'] = True
-else:
+elif st.session_state["submitted"] and st.session_state['signedIn']:
     st.write(st.session_state['output'])
     col1, col2 = st.columns(2)
-
     with col1:
         send = st.button("Send Mail")
-
     with col2:
         notsend = st.button("Redo")
-
     if send:
-        sendMail(st.session_state['to_email'],
-                 st.session_state['output'])
-
+        sendMail(st.session_state['to_email'], st.session_state['output'])
     if notsend:
         st.session_state['submitted'] = False
